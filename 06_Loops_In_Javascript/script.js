3e340ec85f399f1bc18bda487f679f57d1e7bb75
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-05-05 17:03:17 +0530

    Basics loops in javascript

---
+++ 06_Loops_In_Javascript/script.js
@@ -0,0 +1,16 @@
+// Loops in Javascript
+// for, while, do-while
+
+console.log("\nFor loop in JavaScript");
+// for loop in javascript
+for (let i = 1; i <= 10; i++) {
+  console.log("Arbaz Ansari", i);
+}
+
+console.log("\n\nWhile loop in JavaScript");
+// while loop
+let i = 1;
+while (i <= 10) {
+  console.log("Arbaz Ansari", i);
+  i++;
+}
