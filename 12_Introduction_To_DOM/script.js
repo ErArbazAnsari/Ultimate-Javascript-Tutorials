8adb026acb4b76a426a394bb857b80506576f346
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-05-05 22:47:00 +0530

    element selector by id

---
+++ 12_Introduction_To_DOM/script.js
@@ -0,0 +1,12 @@
+// Introduction to DOM manipulation in javascript Programming language.
+
+// alert("Hi! bro this is an alert from javascript.")
+
+//if you want to take input from user
+const data = prompt("Enter you data.");
+console.log("user enter this:", data);
+
+// window.console.log(window.document);
+console.log(document);
+document.title = "My Title";
+document.write(data);
