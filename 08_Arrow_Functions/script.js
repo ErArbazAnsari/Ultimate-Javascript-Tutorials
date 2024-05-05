0fb5a56d7e289a0553d15acbafc8064b7c31c99e
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-05-05 18:24:20 +0530

    arrow functions in javascript

---
+++ 08_Arrow_Functions/script.js
@@ -0,0 +1,57 @@
+const myFirstFunc = () => {
+    console.log("My First Function");
+};
+myFirstFunc();
+
+const mySecFunc = (a, b) => a + b; // One Liner Function
+console.log(mySecFunc(10, 50));
+
+// arguments keyword is only available in the normal function
+function myFunc() {
+    console.log(arguments);
+}
+// myFunc(10, 20, 30);
+
+// using return function
+// const myNewFunc = () => {
+//     console.log(arguments);
+// };
+// myNewFunc(10, 29, 30);
+
+// To solve this issue --> we use spread operator
+const myNewFunc = (...arg) => {
+    console.log(arg);
+};
+myNewFunc(10, 29, 30);
+
+// Another difference in return function is hoisting
+// in normal function --> works normally
+sum(10, 20);
+
+function sum(a, b) {
+    console.log(a + b);
+}
+
+// in return funtion
+// div(a, b);
+// const div = (a, b) => a / b;
+
+// Another Difference is this keyword
+// const myObj = {
+//     value: 20,
+//     myFunction: function () {
+//         console.log("Hi, dear: ", this.value);
+//     },
+// };
+// myObj.myFunction();
+
+// In Return Function
+// Another Difference is this keyword
+const myObj = {
+    value: 20,
+    myFunction: () => {
+        console.log("Hi, dear: ", this.value);
+    },
+};
+
+myObj.myFunction();
